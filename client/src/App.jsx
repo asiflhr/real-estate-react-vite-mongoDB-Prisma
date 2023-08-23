e71cc228f26fc31ee701d570833d228e38475e3b
Muhammad Asif <asif88383@gmail.com>
2023-08-24 00:00:47 +0500

    'Property-page-added'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,42 @@
-import React, { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
+import Layout from './components/Layout/Layout'
+
 import Website from './pages/Website'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout/Layout'
 import Properties from './pages/Properties/Properties'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import PuffLoader from 'react-spinners/PuffLoader'
+import Property from './pages/Property/Property'
+// import UserDetailContext from './context/UserDetailContext'
+// import Bookings from './pages/Bookings/Bookings'
+// import Favourites from './pages/Favourites/Favourites'
 
 function App() {
   const queryClient = new QueryClient()
+
+  const [userDetails, setUserDetails] = useState({
+    favourites: [],
+    bookings: [],
+    token: null,
+  })
+
   return (
+    // <UserDetailContext.Provider value={{ userDetails, setUserDetails }}>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Suspense
-          fallback={
-            <div className='wrapper flexCenter' style={{ height: '60vh' }}>
-              <PuffLoader
-                color='#4066ff'
-                height={100}
-                width={100}
-                radius={1}
-                aria-label='puff-loading'
-              />
-            </div>
-          }
-        >
+        <Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route element={<Layout />}>
               <Route path='/' element={<Website />} />
-              <Route path='/properties' element={<Properties />} />
+              <Route path='/properties'>
+                <Route index element={<Properties />} />
+                <Route path=':propertyId' element={<Property />} />
+              </Route>
+              {/* <Route path='/bookings' element={<Bookings />} /> */}
+              {/* <Route path='/favourites' element={<Favourites />} /> */}
             </Route>
           </Routes>
         </Suspense>
@@ -39,6 +44,7 @@ function App() {
       <ToastContainer />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
+    // </UserDetailContext.Provider>
   )
 }
 
