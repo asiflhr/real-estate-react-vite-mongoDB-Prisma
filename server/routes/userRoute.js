0d303f969f5c07439b3b04e40450289e0106c68d
Muhammad Asif <asif88383@gmail.com>
2023-08-24 23:29:25 +0500

    'configured-vercel'

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,21 +1,19 @@
-import express from 'express'
+import express from "express";
 import {
-  createUser,
   bookVisit,
   cancelBooking,
+  createUser,
   getAllBookings,
   getAllFavorities,
   toFav,
-} from '../controllers/userCntrl.js'
-import jwtCheck from '../config/auth0Config.js'
+} from "../controllers/userCntrl.js";
+import jwtCheck from "../config/auth0Config.js";
+const router = express.Router();
 
-const router = express.Router()
-
-router.post('/register', createUser)
-router.post('/bookVisit/:id', bookVisit)
-router.post('/allBookings', getAllBookings)
-router.post('removeBooking/:id', cancelBooking)
-router.post('toFav/:rid', toFav)
-router.post('allFav', getAllFavorities)
-
-export { router as userRoute }
+router.post("/register", jwtCheck, createUser);
+router.post("/bookVisit/:id", jwtCheck, bookVisit);
+router.post("/allBookings", getAllBookings);
+router.post("/removeBooking/:id", jwtCheck, cancelBooking);
+router.post("/toFav/:rid", jwtCheck, toFav);
+router.post("/allFav/", jwtCheck, getAllFavorities);
+export { router as userRoute };
