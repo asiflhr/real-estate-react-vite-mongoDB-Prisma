54a7a97ace4803da1a9527f359d8d2ffe4785969
Muhammad Asif <asif88383@gmail.com>
2023-09-07 16:14:00 -0400

    'fixed-undefined-bugs'

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -2,7 +2,7 @@ import { auth } from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
   // audience: 'http:///localhost:8000',
-  audience: 'https://client-real-estate-react-vite-mongo-db-prisma.vercel.app/',
+  audience: 'https://server-real-estate-react-vite-mongo-db-prisma.vercel.app/',
   issuerBaseURL: 'dev-pzsfvqymw322gi3g.us.auth0.com',
   tokenSigningAlg: 'RS256',
 })
